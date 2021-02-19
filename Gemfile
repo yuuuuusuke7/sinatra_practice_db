c76b06c78521c6c27dd7159a573441a09144e61e
Yusuke <yusuke.motegi1119@gmail.com>
2021-02-19 23:32:12 +0900

    「WebアプリからのDB利用」プラクティス課題first_commit

---
+++ Gemfile
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+source 'https://rubygems.org'
+gem 'pg'
+gem 'sinatra'
+gem 'sinatra-contrib'
+
+git_source(:github) { |repo_name| "https://github.com/#{repo_name}" }
---
+++ app.rb
@@ -0,0 +1,86 @@
+# frozen_string_literal: true
+
+require 'sinatra'
+require 'sinatra/reloader'
+require 'pg'
+
+class Memo
+  @connection = PG.connect(dbname: 'memoapp')
+  @connection.prepare('all', 'SELECT * FROM posts')
+  @connection.prepare('create', 'INSERT INTO posts(title, body) VALUES ($1, $2)')
+  @connection.prepare('show', 'SELECT * FROM posts WHERE id = $1')
+  @connection.prepare('update', 'UPDATE posts SET (title, body) = ($1, $2)')
+  @connection.prepare('destroy', 'DELETE FROM posts WHERE id = $1')
+
+  def self.all_memos
+    @connection.exec_prepared('all')
+  end
+
+  def self.create_memo(title: memo_title, body: memo_body)
+    @connection.exec_prepared('create', [title, body])
+  end
+
+  def self.show_memo(id: memo_id)
+    @connection.exec_prepared('show', [id])
+  end
+
+  def self.update_memo(title: memo_title, body: memo_body)
+    @connection.exec_prepared('update', [id, title, body])
+  end
+
+  def self.destroy_memo(id: memo_id)
+    @connection.exec_prepared('destroy', [id])
+  end
+end
+
+get '/' do
+  redirect '/memos'
+end
+
+get '/memos' do
+  @title_head = 'top'
+  @memo = Memo.all_memos
+  erb :index
+end
+
+get '/memos/new' do
+  @title_head = 'new'
+  erb :new
+end
+
+get '/memos/:id' do
+  @title_head = 'show'
+  @memo = Memo.show_memo(id: params[:id]).first
+  erb :show
+end
+
+post '/memos' do
+  Memo.create_memo(title: h(params[:title]), body: h(params[:body]))
+  redirect '/'
+end
+
+get '/memos/:id/edit' do
+  @title_head = 'edit'
+  @memo = Memo.show_memo(id: params[:id]).first
+  erb :edit
+end
+
+put '/memos/:id' do
+  Memo.update_memo(id: params[:id], title: params[:title], body: params[:body])
+  redirect "/memos/#{params[:id]}"
+end
+
+delete '/memos/:id' do
+  Memo.destroy_memo(id: params[:id])
+  redirect '/'
+end
+
+not_found do
+  'ファイルが存在しません'
+end
+
+helpers do
+  def h(str)
+    Rack::Utils.escape_html(str)
+  end
+end
